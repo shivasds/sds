18526841c9500d1357a3170f1b4d4f4a33a1c2ad
shiva prasad <shiva@secondsdigital.com>
2020-04-04 15:23:00 +0530

    COnfig 3

--- index.php
+++ index.php
@@ -73,19 +73,16 @@ else
 {
    define('ENVIRONMENT', 'production');
 }
-
 switch (ENVIRONMENT)
 {
 	case 'development':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
-		ini_set('memory_limit', '1200M');
 	break;
 
 	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
-		ini_set('memory_limit', '1200M');
 		if (version_compare(PHP_VERSION, '5.3', '>='))
 		{
 			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
