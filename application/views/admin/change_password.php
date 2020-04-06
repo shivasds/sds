15661d1dcd227b4f60633ddf36999f05e6f3b1ca
preetika <shiva@seconddigital.com>
2020-04-06 14:26:57 +0530

    change password validation

--- application/views/admin/change_password.php
+++ application/views/admin/change_password.php
@@ -1,6 +1,42 @@
 <?php $this->load->view('admin/inc/header');?>
 <style type="text/css">
  
+/* The message box is shown when the user clicks on the password field */
+#message {
+  display:none;
+  background: #f1f1f1;
+  color: #000;
+  position: relative;
+  padding: 20px;
+  margin-top: 10px;
+}
+
+#message p {
+  padding: 10px 35px;
+  font-size: 18px;
+}
+
+/* Add a green text color and a checkmark when the requirements are right */
+.valid {
+  color: green;
+}
+
+.valid:before {
+  position: relative;
+  left: -35px;
+  content: "✔";
+}
+
+/* Add a red text color and an "x" when the requirements are wrong */
+.invalid {
+  color: red;
+}
+
+.invalid:before {
+  position: relative;
+  left: -35px;
+  content: "✖";
+}
  </style>
    <link rel='stylesheet' id='forms-css' href='<?=base_url();?>assets/wp-content/admin/reset.css' type='text/css' media='all' />
 <body id="page-top">
@@ -46,26 +82,26 @@
                             <h3 class="box-title"><?=$heading;?></h3>
                         </div>
                            <?php
-    if ($this->session->flashdata('success')) {
-        ?>
-        <div class="alert alert-success">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <strong style="color: #3c763d;"><i class="fa fa-save" aria-hidden="true"></i></strong> <span
-                    style="color: #3c763d;"><?= $this->session->flashdata('success') ?></span>
-        </div>
-        <?php
-    }
-    if ($this->session->flashdata('error')) {
-        ?>
-        <div class="alert alert-danger">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            <strong style="color: #a94442;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong> <span
-                    style="color: #a94442;"><?= $this->session->flashdata('error') ?></span>
-        </div>
-        <?php
-    }
+                              if ($this->session->flashdata('success')) {
+                                  ?>
+                                  <div class="alert alert-success">
+                                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                      <strong style="color: #3c763d;"><i class="fa fa-save" aria-hidden="true"></i></strong> <span
+                                              style="color: #3c763d;"><?= $this->session->flashdata('success') ?></span>
+                                  </div>
+                                  <?php
+                              }
+                              if ($this->session->flashdata('error')) {
+                                  ?>
+                                  <div class="alert alert-danger">
+                                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                                      <strong style="color: #a94442;"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i></strong> <span
+                                              style="color: #a94442;"><?= $this->session->flashdata('error') ?></span>
+                                  </div>
+                                  <?php
+                              }
 
-    ?>
+                              ?>
                         <!-- /.box-header -->
                         <hr>
                         <!-- form start -->
@@ -73,18 +109,32 @@
   
                           <div class="modal-body">
                             <div class="signin-form reset-password">
-                               <form action="#" method="post">
-                                <input type="password" style="color:black" placeholder="Password" name="changepswd" required="">
-                                <input type="password"style="color:black" placeholder="Repeat Password" name="changepswd2" required="">
+                               <form action="#" method="post" onSubmit = "return checkPassword(this)">
+                                <input type="password" style="color:black" id="password" placeholder="Password" name="changepswd" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                                           title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+                                <input type="password"style="color:black" id="confirm_password" placeholder="Repeat Password" name="changepswd2" required="">
                                 <input type="submit" class="send" value="Update Password">
                               </form>
-                          
+                              </div>
+
+                              <div id="message">
+                                <h3>Password must contain the following:</h3>
+                                <p id="letter" class="invalid">A <b>lowercase</b> letter</p>
+                                <p id="capital" class="invalid">A <b>capital (uppercase)</b> letter</p>
+                                <p id="number" class="invalid">A <b>number</b></p>
+                                <p id="length" class="invalid">Minimum <b>8 characters</b></p>
+                              </div>
+                                  
+                          </div>
                         </form>
-                    </div>
+                  
                     <div class="clearfix"></div><br><br><br>
                     <!-- /.box -->
-                </div>
+               
             </div>
+            </div>
+            </div>
+
         </section> 
         <!-- ADD City end -->
   
@@ -134,10 +184,89 @@
   <script src="../assets/js/dashboard.min.js"></script>
 
 
-  <script type="text/javascript">
-    $(document).ready(function(){
-        $('[data-toggle="tooltip"]').tooltip();
-    });
+  <script> 
+          var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
+
+     function validatePassword(){
+      if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+    }
+
+    password.onchange = validatePassword;
+    confirm_password.onkeyup = validatePassword;
+        </script> 
+
+<script>
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
+var password = document.getElementById("password");
+var confirm_password = document.getElementById("confirm_password");
+
+
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
+
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+
+  if(password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Passwords Don't Match");
+      } else {
+        confirm_password.setCustomValidity('');
+      }
+}
 </script>
 </body>
 
