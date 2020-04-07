5b978005b0cef7c6faae4a70e8c757d7d040753a
preetika <shiva@seconddigital.com>
2020-04-07 15:02:40 +0530

    blog page addition

--- application/views/blog.php
+++ application/views/blog.php
@@ -1,6 +1,6 @@
 <?php $this->load->view('inc/header');?>
 <link type=text/css media=all href=<?=base_url()?>assets/wp-content/cache/autoptimize/css/blog.css rel=stylesheet>
-<!-- <link rel=stylesheet id=ms-custom-css href='assets/wp-content/cache/autoptimize/css/common.css' type=text/css media=all> -->
+<link rel=stylesheet id=ms-custom-css href=<?=base_url()?>assets/wp-content/cache/autoptimize/css/blog-content.css type=text/css media=all>
    
 
 <body class="">
@@ -12,16 +12,82 @@
                     <div class=site-main>
                         <div id=dslc-main>
                             <div class="dslc-modules-section " style="padding-bottom:0px;padding-top:0px;border-right-style: hidden; border-left-style: hidden; " data-section-id=065150a228a>
-                                <div class="dslc-modules-section-wrapper dslc-clearfix">
-                                    <div class="dslc-modules-area dslc-col dslc-12-col dslc-last-col" data-size=12>
-                                        <div id=dslc-module-1140 class="dslc-module-front dslc-module-DSLC_Separator dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular " >
-                                            <div class=dslc-separator-wrapper>
-                                                <div class="dslc-separator dslc-separator-style-invisible"></div>
-                                                <div></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                  
+                             
+                            <section class="section mt-5" id="bog_test">
+								<div class="container">
+								
+									<div class="row">
+									   <div class="col-md-6">
+										  <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+											 <div class="card-body d-flex flex-column align-items-start">
+												<strong class="d-inline-block mb-2 text-primary">World</strong>
+												<h6 class="mb-0">
+												   <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
+												</h6>
+												<div class="mb-1 text-muted small">April 2020</div>
+												<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+												<a class="btn btn-outline-primary btn-sm" role="button" href="#">Continue reading</a>
+											 </div>
+											 <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="assets/wp-content/uploads/services-web-development-380x380.png" style="width: 200px; height: 250px;">
+										  </div>
+									   </div>
+                                       <div class="col-md-6">
+										  <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+											 <div class="card-body d-flex flex-column align-items-start">
+												<strong class="d-inline-block mb-2 text-primary">World</strong>
+												<h6 class="mb-0">
+												   <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
+												</h6>
+												<div class="mb-1 text-muted small">April 2020</div>
+												<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+												<a class="btn btn-outline-primary btn-sm" role="button" href="#">Continue reading</a>
+											 </div>
+											 <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="assets/wp-content/uploads/services-web-development-380x380.png" style="width: 200px; height: 250px;">
+										  </div>
+                                       </div>
+                                       <div class="col-md-6">
+										  <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+											 <div class="card-body d-flex flex-column align-items-start">
+												<strong class="d-inline-block mb-2 text-primary">World</strong>
+												<h6 class="mb-0">
+												   <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
+												</h6>
+												<div class="mb-1 text-muted small">April 2020</div>
+												<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+												<a class="btn btn-outline-primary btn-sm" role="button" href="#">Continue reading</a>
+											 </div>
+											 <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="assets/wp-content/uploads/services-web-development-380x380.png" style="width: 200px; height: 250px;">
+										  </div>
+                                       </div>
+                                       <div class="col-md-6">
+										  <div class="card flex-md-row mb-4 shadow-sm h-md-250">
+											 <div class="card-body d-flex flex-column align-items-start">
+												<strong class="d-inline-block mb-2 text-primary">World</strong>
+												<h6 class="mb-0">
+												   <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
+												</h6>
+												<div class="mb-1 text-muted small">April 2020</div>
+												<p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+												<a class="btn btn-outline-primary btn-sm" role="button" href="#">Continue reading</a>
+											 </div>
+											 <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="assets/wp-content/uploads/services-web-development-380x380.png" style="width: 200px; height: 250px;">
+										  </div>
+									   </div>
+									</div>
+
+									<hr>
+
+								
+								
+							
+									<hr>
+									
+								
+								 </div>
+												
+							  </section>
+
+     
 
 
 
@@ -31,71 +97,9 @@
 
 
 
-                                    
-                                    <div class="dslc-modules-area dslc-col dslc-12-col dslc-last-col" data-size=12>
-                                        <div id=dslc-module-8169 class="dslc-module-front dslc-module-DSLC_Separator dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular " >
-                                            <div class=dslc-separator-wrapper>
-                                                <div class="dslc-separator dslc-separator-style-invisible"></div>
-                                                <div></div>
-                                            </div>
-                                        </div>
-                                    </div>
                                 </div>
                             </div>
-                            <div class="dslc-modules-section " style="padding-bottom:0px;padding-top:0px;background-color:rgb(241, 241, 241);border-right-style: hidden; border-left-style: hidden; " data-section-id=7cc8bf97f60>
-                                <div class="dslc-modules-section-wrapper dslc-clearfix">
-                                    <div class="dslc-modules-area dslc-col dslc-12-col dslc-last-col" data-size=12>
-                                        <div id=dslc-module-8201 class="dslc-module-front dslc-module-DSLC_Separator dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular " >
-                                            <div class=dslc-separator-wrapper>
-                                                <div class="dslc-separator dslc-separator-style-invisible"></div>
-                                                <div></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="dslc-modules-area dslc-col dslc-12-col dslc-last-col" data-size=12>
-                                        <div id=dslc-module-1161 class="dslc-module-front dslc-module-DSLC_Html dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular ">
-                                            <div class=dslc-html-module-content>
-                                                <div class="dslc-widget widget_search" id=search-page>
-                                                    <form role=search action="" class=searchform id=searchform method=get>
-                                                        <label class=screen-reader-text for=s>Search</label>
-                                                        <input type=search placeholder="Search …" id=s value name=s class=field>
-                                                        <input type=submit value=Search id=searchsubmit class="submit button">
-                                                    </form>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div id=dslc-module-1162 class="dslc-module-front dslc-module-DSLC_Text_Simple dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular ">
-                                            <div class=dslc-text-module-content>
-                                                <p>Find what you're looking for by browsing the links below or searching up above.</p>
-                                            </div>
-                                        </div>
-                                        <div id=dslc-module-1163 class="dslc-module-front dslc-module-DSLC_Widgets dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular " >
-                                            <div class="dslc-widgets dslc-clearfix dslc-widgets-4-col">
-                                                <div class="dslc-widgets-wrap dslc-clearfix">
-                                                    <div id=categories-5 class="dslc-widget dslc-col widget_categories">
-                                                        <div class=dslc-widget-wrap>
-                                                            <h3 class="dslc-widget-title">
-                                                                <span class=dslc-widget-title-inner>Categories</span></h3>
-                                                            <ul>
-                                                              <span class="dslc-icon"><i class="far fa-bookmark"></i>&emsp;No categories</span>
-                                                            </ul>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="dslc-modules-area dslc-col dslc-12-col dslc-last-col" data-size=12>
-                                        <div id=dslc-module-8196 class="dslc-module-front dslc-module-DSLC_Separator dslc-in-viewport-check dslc-in-viewport-anim-none  dslc-col dslc-12-col dslc-last-col  dslc-module-handle-like-regular " >
-                                            <div class=dslc-separator-wrapper>
-                                                <div class="dslc-separator dslc-separator-style-invisible"></div>
-                                                <div></div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        
                     </div>
                     <?php $this->load->view('inc/footer');?>
                 </div>
